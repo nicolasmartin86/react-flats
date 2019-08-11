95e51654ab851cc05c68c0891f413df0498fbc1a
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-08-11 11:24:19 +0200

    initializing first react architecture steps of the airbnb-like app

---
+++ src/components/google_map.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class GoogleMap extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+      </div>
+    );
+  }
+}
+
+export default GoogleMap;
